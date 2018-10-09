37969998df8f76590dab9bfdbc47c0a779d600ee
sushi14kk <sushi14kk@i.softbank.jp>
2018-10-09 18:10:22 +0900

    first commit

---
+++ Assets/Load.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load : MonoBehaviour
+{
+
+    float TimeCount = 5;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        TimeCount -= Time.deltaTime;
+
+        if(TimeCount <= 0)
+        {
+
+                Application.LoadLevel("Smartphone_UI_2_Unity");
+
+        }
+
+	}
+
+}
